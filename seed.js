5b30bbae539dccb21a817ff3e2f39c6d252a5345
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-03-14 13:05:09 -0700

    fixed issue with seed

--- seed.js
+++ seed.js
@@ -1,18 +1,19 @@
 'use strict';
 
-require('dotenv').config;
+require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL);
-
+console.log(process.env.DB_URL);
 const Book = require("./models/book.js")
 
 async function seed(){
 
   await Book.create({
-    title: 'The Mote in God\'s Eye',
+    title: 'The Mote in Gods Eye',
     description: 'A Scifi book',
     status: 'Read' 
   });
+  console.log('success');
   await Book.create({
     title: 'Dune',
     description: 'A Scifi book',
--- server.js
+++ server.js
@@ -1,34 +1,61 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const Book = require('./models/book.js');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Book = require("./models/book.js");
 
 // add validation to confirm we are wired up to our mongo DB
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('Mongoose is connected');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Mongoose is connected");
 });
 
 mongoose.connect(process.env.DB_URL);
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 
-app.get('/books', getBooks);
-async function getBooks(req, res, next){
-  
+app.get("/books", getBooks);
+app.post("/books", postBooks);
+app.delete("/books/:id", deleteBooks);
+
+async function getBooks(req, res, next) {
+  try {
+    let results = await Book.find({});
+    res.status(200).send(results);
+  } catch (err) {
+    next(err);
+  }
 }
 
-app.get('/test', (request, response) => {
+async function postBooks(req, res, next) {
+  try {
+    let createdBook = await Book.create(req.body);
+    res.status(200).send(createdBook);
+  } catch (err) {
+    next(err);
+  }
+}
 
-  response.send('test request received')
+async function deleteBooks(req, res, next) {
+  try {
+    let id = req.params.id;
+    await Book.findByIdAndDelete(id);
+    res.status(200).send(id);
+  } catch (err) {
+    next(err);
+  }
+}
 
-})
+
+app.get("/test", (request, response) => {
+  response.send("test request received");
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
