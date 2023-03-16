881b8231638afb2c3139437741b50e0a701d0b42
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-03-16 14:17:18 -0700

    done with lab15

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Book = require("./models/book.js");
+const verifyUser = require("./auth");
 
 // add validation to confirm we are wired up to our mongo DB
 const db = mongoose.connection;
@@ -27,44 +28,74 @@ app.delete("/books/:id", deleteBooks);
 app.put("/books/:id", putBooks);
 
 async function getBooks(req, res, next) {
-  try {
-    let results = await Book.find({});
-    res.status(200).send(results);
-    console.log('we got books');
-  } catch (err) {
-    next(err);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send("invalid token");
+    } else {
+      try {
+        let results = await Book.find({});
+        res.status(200).send(results);
+        console.log("we got books");
+      } catch (err) {
+        next(err);
+      }
+    }
+  });
 }
 
 async function postBooks(req, res, next) {
-  try {
-    let createdBook = await Book.create(req.body);
-    res.status(200).send(createdBook);
-  } catch (err) {
-    next(err);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send("invalid token");
+    } else {
+      try {
+        let createdBook = await Book.create(req.body);
+        res.status(200).send(createdBook);
+      } catch (err) {
+        next(err);
+      }
+    }
+  });
 }
 
 async function deleteBooks(req, res, next) {
-  try {
-    let id = req.params.id;
-    await Book.findByIdAndDelete(id);
-    res.status(200).send(id);
-  } catch (err) {
-    next(err);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send("invalid token");
+    } else {
+      try {
+        let id = req.params.id;
+        await Book.findByIdAndDelete(id);
+        res.status(200).send(id);
+      } catch (err) {
+        next(err);
+      }
+    }
+  });
 }
 async function putBooks(req, res, next) {
-  try {
-    let id = req.params.id;
-    let updatedBook = req.body;
-    let updatedBookFromDB = await Book.findByIdAndUpdate(id, updatedBook, {new:true, overwrite: true});
-    res.status(200).send(updatedBookFromDB);
-  } catch (err) {
-    next(err);
-  }
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send("invalid token");
+    } else {
+      try {
+        let id = req.params.id;
+        let updatedBook = req.body;
+        let updatedBookFromDB = await Book.findByIdAndUpdate(id, updatedBook, {
+          new: true,
+          overwrite: true,
+        });
+        res.status(200).send(updatedBookFromDB);
+      } catch (err) {
+        next(err);
+      }
+    }
+  });
 }
-
 
 app.get("/test", (request, response) => {
   response.send("test request received");
